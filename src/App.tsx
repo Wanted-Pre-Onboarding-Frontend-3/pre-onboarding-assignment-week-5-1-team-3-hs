4d4265d992d2a72b1008aa500103b7444f9e49d7
ycs1m1yk <satoly4@gmail.com>
2022-09-28 16:10:11 +0900

    chore: remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
-import { Text } from '@/component/base-component/text';
-import { colors } from '@/styles/colors';
-
 const GlobalStyle = createGlobalStyle`
   ${reset}
 `;
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Text color={colors.error500}>test</Text>
-  </>
-);
+const App = () => <GlobalStyle />;
 
 export default App;
